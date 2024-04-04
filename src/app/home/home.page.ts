03492e302c14ecc793f7d2e3b0734d1a287f75c9
GuiBaioni <guilhermebaioni35@gmail.com>
2024-04-04 15:47:22 -0300

    AppImposto

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,9 +9,12 @@ export class HomePage {
 
   P=''; //preço
   R=''; //Região
-  IA=0; //imposto adicionado
-  PF=0; //Prço final
+  IA=0; //imposto adicionado em dolar
+  PF=0; //Prço final em dolar
   TD=0; // tempo de demora
+  D=''; // valor do dolar
+  IR=0; // Imposto adicionado em real
+  PFR=0; // Preço final em real
 
   constructor() {}
 
@@ -19,36 +22,29 @@ export class HomePage {
   Calcular(){
     if (parseFloat(this.P) < 50) {
 this.IA=0;
+this.IR=0;
     }else{
-      if (parseFloat(this.P) < 100 ){
+      if (parseFloat(this.P) <=100 ){
         this.IA=parseFloat(this.P)*0.3;
+        this.IR=this.IA*parseFloat(this.D);
       }else{
-        if (parseFloat(this.P) > 100.1){
-          this.IA=parseFloat(this.P)*0.5;
+       this.IA=parseFloat(this.P)*0.5;
+       this.IR=this.IA*parseFloat(this.D)
         }
-      }
-    }
+      } this.PF= parseFloat(this.P)+this.IA;
+      this.PFR= this.PF*parseFloat(this.D)
 
-    this.PF= parseFloat(this.P)+this.IA;
-
-    if (this.R == "Sudeste") {
-      this.TD = 10;
-    }else {
-      if (this.R == "Sul"){
+      if (this.R.toUpperCase() === 'SUDESTE' || this.R.toUpperCase() === 'SUL' ) {
         this.TD = 10;
-      }else {
-        if (this.R == "Centro-Oeste") {
-          this.TD = 15;
-        }else {
-          if (this.R == "Nordeste") {
-            this.TD = 15;
-      }else {
-        if (this.R == "Norte") {
-          this.TD = 20;
-    }
+      } else if (this.R.toUpperCase() === 'NORDESTE' || this.R.toUpperCase() === 'CENTRO-OESTE' ){
+        this.TD = 15;
+      }
+      else if (this.R.toUpperCase() === 'NORTE'){
+        this.TD = 20;
+      }
+      }
+      }
 
 
-  }
-}
-  }
-} }}
+
+
